2bd49bc2505f71cc879eef2699fceb57045d46cc
Abdur Razzaq <abdurrazaq789@gmail.com>
2025-03-12 23:51:32 +0500

    lecture 05 | 21 React Fragments | Completed

--- src/index.js
+++ src/index.js
@@ -74,20 +74,26 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+      
       {numPizzas > 0 ? (
+        <>
+        <p>
+        Authentic Italian cuisine. 6 creative dishes to choose from. All from
+        our stone oven, all organic, all delicious.
+      </p>
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      </>
       ) : (
-      <p>We're still working on our menu. Please come back later :) </p>
-    )
-    }
+        <p>We're still working on our menu. Please come back later :) </p>
+      )}
     </main>
   );
 }
-function Pizza({pizzaObj}) {
+function Pizza({ pizzaObj }) {
   if (pizzaObj.soldOut) return null;
   return (
     <li className="pizza">
@@ -100,7 +106,6 @@ function Pizza({pizzaObj}) {
     </li>
   );
 }
-
 
 function Footer() {
   const hour = new Date().getHours();
