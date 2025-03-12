b2b5a4b5f7b167de40f7b0dd70c5bac7d65e87d1
Abdur Razzaq <abdurrazaq789@gmail.com>
2025-03-12 16:37:50 +0500

    lecture 17 | Completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css'
+import "./index.css";
 
 // eslint-disable-next-line
 const pizzaData = [
@@ -49,58 +49,73 @@ const pizzaData = [
 ];
 // eslint-disable-next-line
 
-
 function App() {
   return (
     <div className="container">
-      <Header/>
-      <Menu/>
-      <Footer/>
+      <Header />
+      <Menu />
+      <Footer />
     </div>
-  )
+  );
 }
 
-function Header(){
-  // const style = {color: 'white', background: 'gray', fontSize: '44px', textTransform: 'uppercase', padding: '10px'}
+function Header() {
   return (
-  <header className="header footer">
-    <h1>Fast React Pizza Co.</h1>
-  </header>
-  )
+    <header className="header footer">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
-function Menu(){
+function Menu() {
+  const pizzas = pizzaData
+  // const pizzas = [];
+  const numPizzas = pizzas.length
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza/>
-      <Pizza/>
-      <Pizza/>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+        {pizzas.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+      )}
+      
     </main>
-  )
+  );
 }
-function Footer(){
-  const hour = new Date().getHours()
+function Pizza(props) {
+  return (
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>${props.pizzaObj.price + 3}</span>
+      </div>
+    </li>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen)
-
-  // if (hour >= openHour && hour <= closeHour) alert("we are currently Open"); else alert ("sorry we are close");
   return (
-  <footer className="footer">devArcoder | {new Date().toLocaleDateString()} | {new Date().toLocaleTimeString()} | we are currently open!</footer>
-)
+    <footer className="footer">
+      {isOpen && (
+        <div className="order">
+          <p>
+            We're open until {closeHour}:00. Come visit us or order
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      )}
+    </footer>
+  );
 }
 
-
-function Pizza(){
-  return(
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="" />
-      <h3>Pizza</h3>
-      <p>tomato and some spices add in this pizza hut.</p>
-    </div>
-  )
-}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
