c91d01876ca37945691020a127f8eb3f2ea553e6
Abdur Razzaq <abdurrazaq789@gmail.com>
2025-03-12 16:55:55 +0500

    lecture 05 | 17 Conditional Rendering With Ternaries | Continue time 2:25

--- src/index.js
+++ src/index.js
@@ -74,13 +74,16 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
-      )}
+      ) : (
+      <p>We're still working on our menu. Please come back later :) </p>
+    )
+    }
     </main>
   );
 }
